ebddc5416a43de80f1cdb7cfbece541d6f2d4700
AmandaTodero <amanda.todero032@gmail.com>
2023-10-27 13:50:15 -0400

    Set up template for Forecast

--- src/index.js
+++ src/index.js
@@ -14,6 +14,33 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let daily = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+    daily.forEach(function (day) {
+        forecastHTML += ` 
+            <div class="col-2">
+                <div class="weather-forecast-date">
+                    ${day} 
+                </div>
+                <img 
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" 
+                alt="" width="42">
+                <div class="weather-forecast-temperatures">
+                    <span class="weather-forecast-temperature-max">18°</span>
+                    <span class="weather-forecast-temperature-min">12°</span>  
+                </div>                          
+            </div>
+        `;
+    });
+
+    forecastHTML += `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
+
 function displayTemperature(response) {
     console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
@@ -67,7 +94,7 @@ function displayFahrenheitTemperature(event){
 
 }
 
-search("Mooresville");
+
 
 let fahrenheitTemperature = null;
 
@@ -79,3 +106,6 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link"); 
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+displayForecast();
+search("Mooresville");
